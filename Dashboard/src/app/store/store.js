4a40442d21253069a4f6fb8dc0ecf268337ccee6
Yared <yared241993@gmail.com>
2023-11-25 09:39:47 +0300

    add new Dashbord project

---
+++ Dashboard/src/app/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { controllerSlice } from "../../Features/Slice/controllerSlice/controllerSlice";
+
+export default configureStore({
+  reducer: {
+    controller: controllerSlice,
+  },
+});
