ef7593927ed4b0d17af3a38e49df13d366a64605
Yared <yared241993@gmail.com>
2023-11-28 07:51:14 +0300

    add new search input fields

--- Dashboard/src/components/SideBar/SideBar.jsx
+++ Dashboard/src/components/SideBar/SideBar.jsx
@@ -21,6 +21,37 @@ const SideBar = () => {
           </div>
           <div className="flex border h-1 bg-white"></div>
           <ul className="space-y-2 my-5 font-medium">
+            <div className=" flex justify-start items-center py-7 relative">
+              <input
+                className="text-sm leading-none text-left text-gray-600 px-4 py-3 w-full border rounded border-gray-300  outline-none"
+                type="text"
+                placeholder="Search"
+              />
+              <svg
+                className="absolute right-3 z-10 cursor-pointer"
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17Z"
+                  stroke="#4B5563"
+                  strokeWidth="1.66667"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M21 21L15 15"
+                  stroke="#4B5563"
+                  strokeWidth="1.66667"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+
             {/* Dashboard */}
             <li>
               <Link
@@ -94,7 +125,7 @@ const SideBar = () => {
                 </svg>
                 <span className="flex-1 ms-3 whitespace-nowrap">Over View</span>
                 <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">
-                  Pro
+                  new
                 </span>
               </Link>
             </li>
