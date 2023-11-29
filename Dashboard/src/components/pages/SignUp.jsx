313654363238ae5c70cebc4924d849246c7c8867
Yared <yared241993@gmail.com>
2023-11-29 23:47:13 +0300

    modfied validation

--- Dashboard/src/components/pages/SignUp.jsx
+++ Dashboard/src/components/pages/SignUp.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import { Form, useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const handleRegister = ({
+    firstName,
+    lastName,
+    email,
+    password,
+    ConfirmePassword,
+    tel,
+  }) => {
+    console.log(username + "" + password);
+  };
+
   return (
     <div className="h-screen mt-[10vh]">
       <div className="flex  justify-center items-center text-center p-2 mb-4 rounded bg-gray-100">
@@ -11,69 +36,100 @@ const SignUp = () => {
       <form className="max-w-md mx-auto">
         <div className="relative z-0 w-full mb-5 group">
           <input
-            type="email"
-            name="floating_email"
-            id="floating_email"
+            type="text"
+            {...register("firstName", {
+              required: "Enter Your firstName please!",
+            })}
             className=" py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border bg-white  rounded dark:text-white  dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            required
           />
+          {errors.firstName && (
+            <span className="text-red-700 col-span-1 md:col-span-2 ">
+              {errors.firstName.message}
+            </span>
+          )}
         </div>
         <div className="relative z-0 w-full mb-5 group">
           <input
-            type="password"
-            name="floating_password"
-            id="floating_password"
+            type="text"
+            {...register("lastName", {
+              required: "Enter Your lastName please!",
+            })}
             className=" py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border bg-white  rounded appearance-none dark:text-white  dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            required
           />
+          {errors.lastName && (
+            <span className="text-red-700 col-span-1 md:col-span-2 ">
+              {errors.lastName.message}
+            </span>
+          )}
         </div>
+
         <div className="relative z-0 w-full mb-5 group">
           <input
-            type="password"
-            name="repeat_password"
-            id="floating_repeat_password"
+            type="email"
+            {...register("email", {
+              required: "Enter Your Email please!",
+            })}
             className=" py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border  appearance-none dark:text-white bg-white  rounded dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required
+            placeholder="email"
           />
+          {errors.email && (
+            <span className="text-red-700 col-span-1 md:col-span-2 ">
+              {errors.email.message}
+            </span>
+          )}
         </div>
         <div className="grid md:grid-cols-2 md:gap-6">
           <div className="relative z-0 w-full mb-5 group">
             <input
-              type="text"
-              name="floating_first_name"
-              id="floating_first_name"
+              type="password"
+              {...register("password", {
+                required: "Enter Your new password please!",
+              })}
               className=" py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border  dark:text-white bg-white  rounded dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required
+              placeholder="password"
             />
+            {errors.password && (
+              <span className="text-red-700 col-span-1 md:col-span-2 ">
+                {errors.password.message}
+              </span>
+            )}
           </div>
           <div className="relative z-0 w-full mb-5 group">
             <input
-              type="text"
-              name="floating_last_name"
-              id="floating_last_name"
+              type="password"
+              {...register("ConfirmePassword", {
+                required: "Enter Your Correct Password please!",
+              })}
               className=" py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border rounded bg-white dark:text-white  dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required
+              placeholder="ConfirmePassword"
             />
+            {errors.ConfirmePassword && (
+              <span className="text-red-700 col-span-1 md:col-span-2 ">
+                {errors.ConfirmePassword.message}
+              </span>
+            )}
           </div>
         </div>
-        <div className="grid md:grid-cols-2 md:gap-6">
+        <div>
           <div className="relative z-0 w-full mb-5 group">
             <input
               type="tel"
               pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              name="floating_phone"
-              id="floating_phone"
+              {...register("tel", {
+                required: "Enter Your telephone number please!",
+              })}
               className=" py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent  dark:text-white  border rounded bg-white dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required
+              placeholder="Phone Number"
             />
+            {errors.tel && (
+              <span className="text-red-700 col-span-1 md:col-span-2 ">
+                {errors.tel.message}
+              </span>
+            )}
           </div>
-          <div className="relative z-0 w-full mb-5 group">
+          {/* <div className="relative z-0 w-full mb-5 group">
             <input
               type="text"
               name="floating_company"
@@ -82,10 +138,11 @@ const SignUp = () => {
               placeholder=" "
               required
             />
-          </div>
+          </div> */}
         </div>
         <button
           type="submit"
+          onClick={handleSubmit(handleRegister)}
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
         >
           Done!
