127a716d05f1f3d8f042392f6e471397b406642b
Yared <yared241993@gmail.com>
2023-12-06 14:17:21 +0300

    changed the position

--- Dashboard/src/components/pages/SignIn.jsx
+++ Dashboard/src/components/pages/SignIn.jsx
@@ -105,7 +105,7 @@ function SignIn() {
             </div>
             <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
               <img
-                src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                src="https://app.dubbpie.com/assets/images/login-img.png"
                 className="w-full"
                 alt="Sample image"
               />
