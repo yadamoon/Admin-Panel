9a256b54da121871632e94a101a20305fcb81c0c
Yared <yared241993@gmail.com>
2023-12-06 09:53:29 +0300

    updated btn name and change size of btn

--- Dashboard/src/components/pages/SignUp.jsx
+++ Dashboard/src/components/pages/SignUp.jsx
@@ -318,23 +318,23 @@ const SignUp = () => {
                 </div>
 
                 {/* <!-- Login button --> */}
-                <div className="text-center lg:text-left">
+                <div className="text-center  lg:text-left">
                   <button
                     type="button"
-                    className="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal  shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                    className="inline-block rounded bg-gray-400 text-gray-50 px-20  pb-2.5 pt-3 text-sm font-medium uppercase leading-normal"
                     data-te-ripple-init
                     data-te-ripple-color="light"
                   >
-                    Login
+                    Sign UP
                   </button>
 
                   <p className="mb-0 mt-2 pt-1 text-sm font-semibold">
-                    Don't have an account?
+                    I have an account?
                     <a
                       href="#!"
                       className="transition duration-150 ease-in-out  "
                     >
-                      Register
+                      Login
                     </a>
                   </p>
                 </div>
