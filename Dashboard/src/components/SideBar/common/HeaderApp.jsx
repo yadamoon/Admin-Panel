722f08cd6cab08bf3215d53d999c398d28f8d8d2
Yared <yared241993@gmail.com>
2023-12-09 06:53:18 +0300

    updated background-color and updated tailwind part

--- Dashboard/src/components/SideBar/common/HeaderApp.jsx
+++ Dashboard/src/components/SideBar/common/HeaderApp.jsx
@@ -8,43 +8,10 @@ export default function HeaderApp() {
   const [showMenu, setShowMenu] = useState(false);
   return (
     <div className=" 2xl:mx-auto">
-      <div className="bg-white  shadow-lg py-5 px-7">
+      <div className="bg-gray-600   p-4 px-7">
         <nav className="flex justify-between ">
           <div className="mt-8"></div>
-          <div className="lg:w-3/5 flex mt-10 ml-2 lg:ml-0 lg:mt-0 lg:justify-end justify-center items-center">
-            {/* <div className="pt-2 relative  text-gray-600">
-              <input
-                className="focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bg-gray-200 h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                type="search"
-                name="search"
-                placeholder="Search"
-              />
-              <button
-                type="submit"
-                className="focus:ring-2 focus:ring-offset-2 text-gray-600 focus:text-indigo-700 focus:rounded-full   focus:ring-indigo-700 bg-gray-200 e absolute right-0 top-0 mt-5 mr-4"
-              >
-                <svg
-                  className=" h-4 w-4 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  version="1.1"
-                  id="Capa_1"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 56.966 56.966"
-                  style={{ enableBackground: "new 0 0 56.966 56.966" }}
-                  xmlSpace="preserve"
-                  width="512px"
-                  height="512px"
-                >
-                  <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-                </svg>
-              </button>
-            </div> */}
-            {/* <h1 className="font-bold text-teal-600 text-3xl uppercase">
-              food recipe
-            </h1> */}
-          </div>
+          <div className="lg:w-3/5 flex mt-10 ml-2 lg:ml-0 lg:mt-0 lg:justify-end justify-center items-center"></div>
 
           <div className="flex items-center">
             <div className="flex items-center ms-3">
