8ae9243fc713441f5ffb2e1dbb53a996b6334d35
Yared <yared241993@gmail.com>
2023-12-11 17:49:48 +0300

    updated size

--- Dashboard/src/components/pages/SignIn.jsx
+++ Dashboard/src/components/pages/SignIn.jsx
@@ -37,7 +37,7 @@ function SignIn() {
           {/* <!-- Left column container with background--> */}
           <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between ">
             <div className="mb-12 md:mb-0 md:w-8/12 lg:w-2/3 xl:w-2/5 mx-20  shadow-gray-300 shadow-lg rounded ">
-              <form className="space-y-4 md:space-y-6 mx-10">
+              <form className="space-y-4 md:space-y-6 mx-10 my-10 ">
                 {/* <!--Sign in section--> */}
                 <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
                   <p className="mx-4 mb-0 text-center font-bold text-xl">
