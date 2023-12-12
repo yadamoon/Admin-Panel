f579df5a38d222bc660aec6426877340e118e060
Yared <yared241993@gmail.com>
2023-12-12 23:09:40 +0300

    updated sth

--- Dashboard/src/components/pages/ListUser.jsx
+++ Dashboard/src/components/pages/ListUser.jsx
@@ -19,22 +19,3 @@ const ListUser = () => {
 };
 
 export default ListUser;
-
-// import { Pagination } from 'flowbite-react'
-import { useState } from "react";
-
-export function PaginationComponent() {
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const onPageChange = (page) => setCurrentPage(page);
-
-  return (
-    <div className="flex overflow-x-auto sm:justify-center">
-      <Pagination
-        currentPage={currentPage}
-        totalPages={100}
-        onPageChange={onPageChange}
-      />
-    </div>
-  );
-}
