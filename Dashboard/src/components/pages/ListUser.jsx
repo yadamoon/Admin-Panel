ca410f9fff418a5edb12caf15960981d42cb7047
Yared <yared241993@gmail.com>
2023-12-13 17:24:26 +0300

    deleted some code

--- Dashboard/src/components/pages/ListUser.jsx
+++ Dashboard/src/components/pages/ListUser.jsx
@@ -7,13 +7,7 @@ const ListUser = () => {
       className="w-full h-screen
      justify-center items-center"
     >
-      <div className="flex  p-2 mb-4 rounded bg-gray-100">
-        <p className="text-2xl text-gray-400 dark:text-gray-500">
-          List Of Users
-        </p>
-      </div>
       <Table />
-      <div className="mt-10">{/* <PaginationComponent /> */}</div>
     </div>
   );
 };
