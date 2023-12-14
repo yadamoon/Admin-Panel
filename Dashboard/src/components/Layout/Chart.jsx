42f131a9cdbef5c46ed9c49cdd03e831dd932573
Yared <yared241993@gmail.com>
2023-12-14 15:06:20 +0300

    deleted some code

--- Dashboard/src/components/Layout/Chart.jsx
+++ Dashboard/src/components/Layout/Chart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   LineChart,
   Line,
@@ -7,68 +7,56 @@ import {
   YAxis,
   Tooltip,
   Legend,
-} from 'recharts'
+} from "recharts";
 // const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }]
 const data = [
   {
-    name: 'Page A',
+    name: "Page A",
     uv: 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: 'Page B',
+    name: "Page B",
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: 'Page C',
+    name: "Page C",
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
+    name: "Page D",
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
+    name: "Page E",
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
+    name: "Page F",
     uv: 2390,
     pv: 3800,
     amt: 2500,
   },
   {
-    name: 'Page G',
+    name: "Page G",
     uv: 3490,
     pv: 4300,
     amt: 2100,
   },
-]
+];
 
 const Chart = () => {
   return (
     <div className=" flex justify-center items-center">
-      {/* <LineChart
-        width={700}
-        height={350}
-        data={data}
-        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-      >
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-      </LineChart> */}
       <LineChart
         width={1000}
         height={400}
@@ -97,7 +85,7 @@ const Chart = () => {
       <br />
       <br />
     </div>
-  )
-}
+  );
+};
 
-export default Chart
+export default Chart;
