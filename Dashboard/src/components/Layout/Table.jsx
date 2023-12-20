93cfd316597d7356032232082b37813d7977e962
Yared <yared241993@gmail.com>
2023-12-20 17:58:02 +0300

    pass value using props

--- Dashboard/src/components/Layout/Table.jsx
+++ Dashboard/src/components/Layout/Table.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
+import Search from "../pages/Search";
 
 function RCPaginate({ items, itemsPerPage, setter }) {
   const [itemOffset, setItemOffset] = useState(0);
@@ -42,13 +43,14 @@ function Table() {
   const [displayedUsers, setDisplayedUsers] = useState([]);
   // const [recipesDisplayed, setRecipesDisplayed] = useState([]);
 
-  // const [usersFiltered, setUsersFiltered] = useState([]);
+  const [usersFiltered, setUsersFiltered] = useState([]);
   // const [show, setShow] = useState(null);
 
   function getUsers() {
     try {
       axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
         setUsers(res.data);
+        setUsersFiltered(res.data);
 
         console.log(res.data);
       });
@@ -70,48 +72,7 @@ function Table() {
   return (
     <div className="flex justify-center">
       <div className=" w-full">
-        <div className="px-4 xl:px-0 py-10">
-          <div className="flex flex-col lg:flex-row flex-wrap">
-            <div className="mt-4 lg:mt-0 lg:w-3/5">
-              <div>
-                <h1 className="text-2xl ml-2 lg:ml-0 lg:text-3xl font-bold text-gray-500 tracking-normal lg:w-11/12">
-                  List Of Users
-                </h1>
-              </div>
-            </div>
-            <div className="lg:w-2/5 flex mt-10 ml-2 lg:ml-0 lg:mt-0 lg:justify-end">
-              <div className="pt-2 relative  text-gray-600">
-                <input
-                  className="focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                  type="search"
-                  name="search"
-                  placeholder="Search"
-                />
-                <button
-                  type="submit"
-                  className="focus:ring-2 focus:ring-offset-2 text-gray-600 focus:text-indigo-700 focus:rounded-full  focus:bg-gray-100 focus:ring-indigo-700 bg-white focus:outline-none absolute right-0 top-0 mt-5 mr-4"
-                >
-                  <svg
-                    className=" h-4 w-4 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlnsXlink="http://www.w3.org/1999/xlink"
-                    version="1.1"
-                    id="Capa_1"
-                    x="0px"
-                    y="0px"
-                    viewBox="0 0 56.966 56.966"
-                    style={{ enableBackground: "new 0 0 56.966 56.966" }}
-                    xmlSpace="preserve"
-                    width="512px"
-                    height="512px"
-                  >
-                    <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Search users={users} setter={setUsersFiltered} />
         <div className=" flex  ">
           <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             {/* //todo Header of Table */}
