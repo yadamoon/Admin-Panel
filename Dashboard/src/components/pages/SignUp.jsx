412a9cd54629c903b66f015695987f30ecb2f27c
Yared <yared241993@gmail.com>
2023-12-23 15:17:51 +0300

    updated the min and max length

--- Dashboard/src/components/pages/SignUp.jsx
+++ Dashboard/src/components/pages/SignUp.jsx
@@ -125,6 +125,14 @@ const SignUp = () => {
                         value: /[@]/,
                         message: "Enter at least one special character",
                       },
+                      minLength: {
+                        value: 2,
+                        message: "Please Enter atleast more than 2 character",
+                      },
+                      maxLength: {
+                        value: 15,
+                        message: "Please Enter atleast less than 15 character",
+                      },
                     })}
                     className="peer block min-h-[auto] w-full rounded border bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  "
                     placeholder="Email address"
