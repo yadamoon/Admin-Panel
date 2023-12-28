779bcc41c3235bb7423836978086b4229c6126fc
Yared <yared241993@gmail.com>
2023-12-28 17:21:15 +0300

    i changed img then updated hover part and modified width full

--- Dashboard/src/components/pages/Products.jsx
+++ Dashboard/src/components/pages/Products.jsx
@@ -3,37 +3,37 @@ import React from "react";
 const Products = () => {
   const items = [
     {
-      // name: " Account Overview",
-      src: "https://i.ibb.co/27R6nk5/home-1.png",
+      name: " Account Overview",
+      src: "https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph",
       alt: "home-1",
       msg: "",
     },
     {
-      // name: " Subscription Plans",
+      name: " Subscription Plans",
       src: "https://i.ibb.co/bdGyLYk/pricetags-1.png",
       alt: "pricetags-1",
       msg: "",
     },
     {
-      // name: "Payment Options",
+      name: "Payment Options",
       src: "https://i.ibb.co/GT4KHvJ/card-1.png",
       alt: "home-1",
       msg: "",
     },
     {
-      // name: " Notification Settings",
+      name: " Notification Settings",
       src: "https://i.ibb.co/rG4r6NJ/notifications-1.png",
       alt: "home-1",
       msg: "",
     },
     {
-      // name: "Profile Preferences",
+      name: "Profile Preferences",
       src: "https://i.ibb.co/HFC1hqn/people-1.png",
       alt: "home-1",
       msg: "",
     },
     {
-      // name: " Privacy and Cookies",
+      name: " Privacy and Cookies",
       src: "https://i.ibb.co/QX80fYm/lock-closed-1.png",
       alt: "home-1",
       msg: "",
@@ -91,87 +91,18 @@ const Products = () => {
               <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pb-6 gap-8">
                 {items.map((item) => (
                   <div key={item.id} role="cell" className="bg-gray-100">
-                    <div className="bg-white p-5 rounded-md relative h-full w-full">
+                    <div className="bg-white  rounded-md relative h-full w-full">
                       {/* class="absolute inset-0 object-center object-cover h-full w-full"  */}
-                      <span>
+                      <span className="hover:opacity-75">
                         <img
-                          className="bg-gray-200 p-2 mb-5 rounded-full w-full"
+                          className="bg-gray-200 p-2 mb-5  w-full"
                           src={item.src}
                           alt={item.msg}
                         />
                       </span>
-                      <h1 className="pb-4 text-2xl font-semibold">
+                      <h1 className="pb-4 text-2xl font-semibold text-center items-center">
                         {item.name}
                       </h1>
-                      <div className="my-5">
-                        <div className="flex items-center pb-4 dark:border-gray-700 cursor-pointer w-full space-x-3 hover:opacity-60">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="12.5"
-                            height={16}
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth={2}
-                              d="M9 5l7 7-7 7"
-                            />
-                          </svg>
-                          <h4 className="text-md text-gray-900 ">
-                            First time, what do I do next?
-                          </h4>
-                        </div>
-                        <div className="flex items-center pb-4 dark:border-gray-700 cursor-pointer w-full space-x-3">
-                          <div>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="12.5"
-                              height={16}
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M9 5l7 7-7 7"
-                              />
-                            </svg>
-                          </div>
-                          <h4 className="text-md text-gray-900 ">
-                            Changing you profile picture and other information
-                          </h4>
-                        </div>
-                      </div>
-                      <a
-                        className="hover:text-indigo-500 hover:underline absolute bottom-5 text-sm text-indigo-700 font-bold cursor-pointer flex items-center"
-                        href="#"
-                      >
-                        <p>Show All</p>
-                        <div>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="icon icon-tabler icon-tabler-arrow-narrow-right"
-                            width={16}
-                            height={16}
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="#4338CA"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          >
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <line x1={5} y1={12} x2={19} y2={12} />
-                            <line x1={15} y1={16} x2={19} y2={12} />
-                            <line x1={15} y1={8} x2={19} y2={12} />
-                          </svg>
-                        </div>
-                      </a>
                     </div>
                   </div>
                 ))}
