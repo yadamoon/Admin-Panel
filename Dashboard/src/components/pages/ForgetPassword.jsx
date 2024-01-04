9c9c07aa9bea26b3cc5fe8ce3a0438eac3195ef5
Yared <yared241993@gmail.com>
2024-01-04 15:41:05 +0300

    i created new functional compoenent and handle click and one variable using useState

--- Dashboard/src/components/pages/ForgetPassword.jsx
+++ Dashboard/src/components/pages/ForgetPassword.jsx
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ForgetPassword = () => {
+  const [show, setShow] = useState(true);
+
+  const ClickEmail = () => {
+    setShow(false);
+  };
   return (
     <div>
       {/* //!? continer  */}
+      {!show && (
+        <section className="">
+          <div className="flex flex-col items-center   mx-auto md:h-screen lg:py-20">
+            <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md  sm:p-8">
+              <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                Recover Account
+              </h2>
+              <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
+                <div>
+                  <label
+                    for="email"
+                    className="block mb-2 text-sm font-medium text-gray-900  "
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 "
+                    placeholder="name@company.com"
+                    required=""
+                  />
+                </div>
+
+                <button
+                  onClick={ClickEmail}
+                  type="submit"
+                  className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
+                >
+                  Reset passwod
+                </button>
+              </form>
+            </div>
+          </div>
+        </section>
+      )}
+      {show && <CreateNewPassword />}
+    </div>
+  );
+};
+
+export default ForgetPassword;
+
+export const CreateNewPassword = () => {
+  return (
+    <div className="">
       <section className="">
         <div className="flex flex-col items-center   mx-auto md:h-screen lg:py-20">
           <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md  sm:p-8">
@@ -12,22 +64,6 @@ const ForgetPassword = () => {
             </h2>
             <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
               <div>
-                <label
-                  for="email"
-                  className="block mb-2 text-sm font-medium text-gray-900  "
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 "
-                  placeholder="name@company.com"
-                  required=""
-                />
-              </div>
-              {/* <div>
                 <label
                   for="password"
                   className="block mb-2 text-sm font-medium text-gray-900 "
@@ -83,7 +119,7 @@ const ForgetPassword = () => {
                     </a>
                   </label>
                 </div>
-              </div> */}
+              </div>
               <button
                 type="submit"
                 className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
@@ -97,5 +133,3 @@ const ForgetPassword = () => {
     </div>
   );
 };
-
-export default ForgetPassword;
