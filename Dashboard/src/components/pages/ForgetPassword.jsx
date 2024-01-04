f938dcfee0a2066a13201d07b66eb2cec6c1f94d
Yared <yared241993@gmail.com>
2024-01-04 15:45:18 +0300

    updated the css part using tailwindCss

--- Dashboard/src/components/pages/ForgetPassword.jsx
+++ Dashboard/src/components/pages/ForgetPassword.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
 const ForgetPassword = () => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
 
   const ClickEmail = () => {
-    setShow(false);
+    setShow(true);
   };
   return (
     <div>
@@ -75,7 +75,7 @@ export const CreateNewPassword = () => {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 "
                   required=""
                 />
               </div>
@@ -91,7 +91,7 @@ export const CreateNewPassword = () => {
                   name="confirm-password"
                   id="confirm-password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 "
                   required=""
                 />
               </div>
@@ -101,7 +101,7 @@ export const CreateNewPassword = () => {
                     id="newsletter"
                     aria-describedby="newsletter"
                     type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 "
                     required=""
                   />
                 </div>
