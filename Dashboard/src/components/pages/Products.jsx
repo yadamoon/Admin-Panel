610897220432db463e55ce81b1ab2ca130d138d4
Yared <yared241993@gmail.com>
2024-01-04 13:25:21 +0300

    updated sth

--- Dashboard/src/components/pages/Products.jsx
+++ Dashboard/src/components/pages/Products.jsx
@@ -55,6 +55,52 @@ const Products = () => {
                   </div>
                 </div>
 
+                <div>
+                  <ul className="flex flex-col md:flex-row items-start md:items-center text-gray-600 dark:text-gray-400 text-sm mt-3">
+                    <li className="flex items-center mr-4">
+                      <div className="mr-1">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="icon icon-tabler icon-tabler-paperclip"
+                          width={16}
+                          height={16}
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path stroke="none" d="M0 0h24v24H0z" />
+                          <path d="M15 7l-6.5 6.5a1.5 1.5 0 0 0 3 3l6.5 -6.5a3 3 0 0 0 -6 -6l-6.5 6.5a4.5 4.5 0 0 0 9 9 l6.5 -6.5" />
+                        </svg>
+                      </div>
+                      <span>Active</span>
+                    </li>
+                    <li className="flex items-center mr-4 mt-4 md:mt-0">
+                      <div className="mr-1">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="icon icon-tabler icon-tabler-trending-up"
+                          width={16}
+                          height={16}
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path stroke="none" d="M0 0h24v24H0z" />
+                          <polyline points="3 17 9 11 13 15 21 7" />
+                          <polyline points="14 7 21 7 21 14" />
+                        </svg>
+                      </div>
+                      <span> Trending</span>
+                    </li>
+                  </ul>
+                </div>
+
                 <div className="lg:w-2/5 flex mt-10 ml-2 lg:ml-0 lg:mt-0 lg:justify-end">
                   <div className="pt-2 relative  text-gray-600">
                     <input
