5ecd48c36d5ef272972f9397ccb2a3ae627b8733
Yared <yared241993@gmail.com>
2024-01-05 06:59:17 +0300

    updated the name of button

--- Dashboard/src/components/pages/ForgetPassword.jsx
+++ Dashboard/src/components/pages/ForgetPassword.jsx
@@ -124,7 +124,7 @@ export const CreateNewPassword = () => {
                 type="submit"
                 className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
               >
-                Reset passwod
+                Done
               </button>
             </form>
           </div>
