416be8ac27e6419f6d05df6a43a63415e9135aac
Yared <yared241993@gmail.com>
2024-01-05 16:57:42 +0300

    updated for responsive

--- Dashboard/src/components/pages/DashboardApp.jsx
+++ Dashboard/src/components/pages/DashboardApp.jsx
@@ -14,34 +14,34 @@ const DashboardApp = () => {
         </div>
 
         {/* //todo list Earing */}
-        <div className="grid grid-cols-3 gap-4 mb-4">
-          <div className="flex rounded bg-gray-50 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
+          <div className="flex rounded bg-gray-50">
             {/* earing */}
-            <div class="flex flex-col bg-white border border-t-4 border-t-blue-600 shadow-sm rounded-xl  dark:border-t-blue-500 dark:shadow-slate-500/[.7]">
-              <div class="p-4 md:p-5">
-                <h3 class="text-lg font-bold text-gray-800 dark:text-white">
+            <div className="flex flex-col bg-white border border-t-4 border-t-blue-600 shadow-sm rounded-xl dark:border-t-blue-500 dark:shadow-slate-500/[.7]">
+              <div className="p-4 md:p-5">
+                <h3 className="text-lg font-bold text-gray-800 dark:text-white">
                   Card title
                 </h3>
-                <p class="mt-2 text-gray-500 dark:text-gray-400">
+                <p className="mt-2 text-gray-500 dark:text-gray-400">
                   With supporting text below as a natural lead-in to additional
                   content.
                 </p>
                 <a
-                  class="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                  className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                   href="#"
                 >
                   Card link
                   <svg
-                    class="flex-shrink-0 w-4 h-4"
+                    className="flex-shrink-0 w-4 h-4"
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   >
                     <path d="m9 18 6-6-6-6" />
                   </svg>
@@ -50,71 +50,67 @@ const DashboardApp = () => {
             </div>
           </div>
           {/* another */}
-          <div className="flex  rounded bg-gray-50 ">
-            <div class="flex flex-col bg-white border border-t-4 border-t-blue-600 shadow-sm rounded-xl   dark:border-t-blue-500 dark:shadow-slate-500/[.7]">
-              <div class="p-4 md:p-5">
-                <h3 class="text-lg font-bold text-gray-800 dark:text-white">
-                  Card title
-                </h3>
-                <p class="mt-2 text-gray-500 dark:text-gray-400">
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </p>
-                <a
-                  class="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                  href="#"
+          <div className="flex flex-col bg-white border border-t-4 border-t-blue-600 shadow-sm rounded-xl dark:border-t-blue-500 dark:shadow-slate-500/[.7]">
+            <div className="p-4 md:p-5">
+              <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+                Card title
+              </h3>
+              <p className="mt-2 text-gray-500 dark:text-gray-400">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a
+                className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                href="#"
+              >
+                Card link
+                <svg
+                  className="flex-shrink-0 w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
-                  Card link
-                  <svg
-                    class="flex-shrink-0 w-4 h-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="m9 18 6-6-6-6" />
-                  </svg>
-                </a>
-              </div>
+                  <path d="m9 18 6-6-6-6" />
+                </svg>
+              </a>
             </div>
           </div>
           {/* another */}
-          <div className="flex  rounded bg-gray-50 ">
-            <div class="flex flex-col bg-white border border-t-4 border-t-blue-600 shadow-sm rounded-xl dark:border-t-blue-500 dark:shadow-slate-500/[.7]">
-              <div class="p-4 md:p-5">
-                <h3 class="text-lg font-bold text-gray-800 dark:text-white">
-                  Card title
-                </h3>
-                <p class="mt-2 text-gray-500 dark:text-gray-400">
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </p>
-                <a
-                  class="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                  href="#"
+          <div className="flex flex-col bg-white border border-t-4 border-t-blue-600 shadow-sm rounded-xl dark:border-t-blue-500 dark:shadow-slate-500/[.7]">
+            <div className="p-4 md:p-5">
+              <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+                Card title
+              </h3>
+              <p className="mt-2 text-gray-500 dark:text-gray-400">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a
+                className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                href="#"
+              >
+                Card link
+                <svg
+                  className="flex-shrink-0 w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
-                  Card link
-                  <svg
-                    class="flex-shrink-0 w-4 h-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="m9 18 6-6-6-6" />
-                  </svg>
-                </a>
-              </div>
+                  <path d="m9 18 6-6-6-6" />
+                </svg>
+              </a>
             </div>
           </div>
         </div>
@@ -126,29 +122,33 @@ const DashboardApp = () => {
 
         {/* //todo charts */}
 
-        <div className="grid grid-cols-3 gap-4 mb-4">
-          {/* //todo chart */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
+          {/* Chart component */}
           <div className="flex items-center justify-center rounded bg-white col-span-2">
             <p className="text-2xl text-gray-400 dark:text-gray-500">
               <Chart />
             </p>
           </div>
-          {/*//todo  pie chart */}
+          {/* Pie chart component */}
           <div className="flex items-center justify-center rounded bg-white col-span-1">
             <p className="text-2xl text-gray-400 dark:text-gray-500">
-              {" "}
               <PieComponent />
             </p>
           </div>
         </div>
-        <div className="grid grid-cols-3 gap-4">
-          <div className="col-span-2 justify-center items-center border ">
-            {" "}
-            <ProgressBarApp />{" "}
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
+          {/* Chart component */}
+          <div className="flex items-center justify-center rounded bg-white col-span-2">
+            <p className="text-2xl text-gray-400 dark:text-gray-500">
+              <ProgressBarApp />{" "}
+            </p>
           </div>
-          <div className="col-span-1 justify-center items-center border ">
-            {" "}
-            <SpaceWrap />
+          {/* Pie chart component */}
+          <div className="flex items-center justify-center rounded bg-white col-span-1">
+            <p className="text-2xl text-gray-400 dark:text-gray-500">
+              <SpaceWrap />
+            </p>
           </div>
         </div>
       </div>
@@ -160,34 +160,38 @@ export default DashboardApp;
 
 export const SpaceWrap = () => {
   return (
-    <div className="justify-center items-center">
-      <Space wrap>
-        <Progress type="dashboard" percent={75} />
-        <Progress type="dashboard" percent={70} status="exception" />
-        <Progress type="circle" percent={100} />
-        <Progress type="circle" percent={50} />
-      </Space>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 justify-center items-center ">
+      <div className="justify-center items-center">
+        <Space wrap>
+          <Progress type="dashboard" percent={75} />
+          <Progress type="dashboard" percent={70} status="exception" />
+          <Progress type="circle" percent={100} />
+          <Progress type="circle" percent={50} />
+        </Space>
+      </div>
     </div>
   );
 };
 
 export const ProgressBarApp = () => {
   return (
-    <div className="justify-center items-center">
-      <Progress
-        percent={30}
-        className="col-span-1 p-2 flex"
-        status="exception"
-      />
-      <Progress percent={60} className="col-span-1 p-2 flex" />
-      <Progress percent={70} className="col-span-1 p-2 flex" />
-      <Progress percent={40} className="col-span-1 p-2 flex" />
-      <Progress percent={100} className="col-span-1 p-2 flex" />
-      <Progress
-        percent={100}
-        className="col-span-1 p-2 flex"
-        status="success"
-      />
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4 ">
+      <div className="w-full">
+        <Progress
+          percent={30}
+          className="col-span-1 w-80 p-3 flex"
+          status="exception"
+        />
+        <Progress percent={60} className="col-span-1 p-2 flex" />
+        <Progress percent={70} className="col-span-1 p-2 flex" />
+        <Progress percent={40} className="col-span-1 p-2 flex" />
+        <Progress percent={100} className="col-span-1 p-2 flex " />
+        <Progress
+          percent={100}
+          className="col-span-1 p-2 flex"
+          status="success"
+        />
+      </div>
     </div>
   );
 };
