5a5ac084114f0ecfdeb777727743b4ca272a886c
Yared <yared241993@gmail.com>
2024-01-05 07:30:11 +0300

    add the input field for searching

--- Dashboard/src/components/SideBar/common/HeaderApp.jsx
+++ Dashboard/src/components/SideBar/common/HeaderApp.jsx
@@ -370,7 +370,33 @@ export default function IndexPage() {
                         </div>
                       </div>
                     </div>
-                    <ul className="">
+                    <ul className="my-6">
+                      {/*//!? search */}
+                      <div className="relative w-full">
+                        <div className="text-gray-500 absolute ml-4 inset-0 m-auto w-4 h-4">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="icon icon-tabler icon-tabler-search"
+                            width={16}
+                            height={16}
+                            viewBox="0 0 24 24"
+                            strokeWidth={1}
+                            stroke="#A0AEC0"
+                            fill="none"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          >
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <circle cx={10} cy={10} r={7} />
+                            <line x1={21} y1={21} x2={15} y2={15} />
+                          </svg>
+                        </div>
+                        <input
+                          className="bg-gray-100 focus:outline-none rounded w-full text-sm text-gray-500  pl-10 py-2"
+                          type="text"
+                          placeholder="Search"
+                        />
+                      </div>
                       <Link to="/" className="cursor-pointer">
                         <li className="text-gray-800 pt-10">
                           <div className="flex items-center">
@@ -521,7 +547,7 @@ export default function IndexPage() {
                   </div>
                   <div className="w-full pt-4">
                     <div className="flex justify-center mb-4 w-full">
-                      <div className="relative w-full">
+                      {/* <div className="relative w-full">
                         <div className="text-gray-500 absolute ml-4 inset-0 m-auto w-4 h-4">
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -545,7 +571,7 @@ export default function IndexPage() {
                           type="text"
                           placeholder="Search"
                         />
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 </div>
