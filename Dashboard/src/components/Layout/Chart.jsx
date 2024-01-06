904cd1bf845ab3c84c9905337c2bbe1b18c62eea
Yared <yared241993@gmail.com>
2024-01-06 15:37:56 +0300

    updated sth

--- Dashboard/src/components/Layout/Chart.jsx
+++ Dashboard/src/components/Layout/Chart.jsx
@@ -8,37 +8,25 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
-// const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }]
+
 const data = [
   {
-    name: "Page A",
+    name: "P.A",
     uv: 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: "Page B",
+    name: "P.B",
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: "Page C",
+    name: "P.C",
     uv: 2000,
     pv: 9800,
     amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
   },
 ];
 
