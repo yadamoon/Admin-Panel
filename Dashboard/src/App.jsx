59b22cd0227ec2e302f2f62f5397b43561773ac0
yadamoon <yared241993@gmail>
2024-01-11 17:30:43 +0300

    sth updated

--- Dashboard/src/App.jsx
+++ Dashboard/src/App.jsx
@@ -14,6 +14,7 @@ import Error from "./components/pages/Error";
 import ForgetPassword from "./components/pages/ForgetPassword";
 
 function App() {
+  // const
   return (
     <div className="bg-gray-100 w-full  ">
       {/* //!? sidebar */}
