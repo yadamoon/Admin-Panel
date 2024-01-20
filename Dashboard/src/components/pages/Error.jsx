f2a61ba31973693b07ec5ca79941d150253d20ab
yadamoon <yared241993@gmail.com>
2024-01-20 09:42:24 +0300

    updated css part text-color  and my-3

--- Dashboard/src/components/pages/Error.jsx
+++ Dashboard/src/components/pages/Error.jsx
@@ -3,21 +3,21 @@
 const Error = () => {
   return (
     <>
-      <section className="relative z-10 bg-primary py-[120px] bg-white rounded">
+      <section className="relative z-10 bg-primary py-[320px] bg-white rounded my-3 ">
         <div className="container mx-auto">
           <div className="-mx-4 flex">
             <div className="w-full px-4">
               <div className="mx-auto max-w-[400px] text-center">
-                <h2 className="mb-2 text-[50px] font-bold leading-none text-white sm:text-[80px] md:text-[100px]">
+                <h2 className="mb-2 text-[50px] font-bold leading-none sm:text-[80px] md:text-[100px]">
                   404
                 </h2>
-                <h4 className="mb-3 text-[22px] font-semibold leading-tight text-white">
+                <h4 className="mb-3 text-[22px] font-semibold leading-tight ">
                   Oops! That page can’t be found
                 </h4>
 
                 <a
                   href="javascript:void(0)"
-                  className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white transition hover:bg-gray-600"
+                  className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold transition hover:bg-gray-600"
                 >
                   Go To Home
                 </a>
