d1defc0579ccc9766bb5119065016c0f97a51d34
yadamoon <yared241993@gmail.com>
2024-01-21 14:52:07 +0300

    import and called http

--- Dashboard/src/main.jsx
+++ Dashboard/src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import httpInit from './services/http/httpInit.js'
+
 import { Provider } from "react-redux";
 import store from "./app/store/store.js";
+httpInit()
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
