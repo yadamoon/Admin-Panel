530c051efc2bcf4f268d9effd4a282a9cdb6cf85
yadamoon <yared241993@gmail.com>
2024-01-22 18:48:03 +0300

    updated fix some error

--- Dashboard/src/store/Slice/authSlice.js
+++ Dashboard/src/store/Slice/authSlice.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import { createSlice } from "@reduxjs/toolkit";
 import { tokenService, userService } from "../../services/storageService";
 const initialState = {
