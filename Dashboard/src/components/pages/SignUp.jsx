cd26771af55247080251fc8d81380eb00bc6d907
yadamoon <yared241993@gmail.com>
2024-01-22 17:40:21 +0300

    deleted some comment code

--- Dashboard/src/components/pages/SignUp.jsx
+++ Dashboard/src/components/pages/SignUp.jsx
@@ -14,7 +14,7 @@ const SignUp = () => {
     reset,
   } = useForm({
     defaultValues: {
-      firstname :'',
+    firstname :'',
     lastname:'',
     email:'',
     password:''
@@ -54,13 +54,7 @@ const SignUp = () => {
         <div className="h-full">
           {/* <!-- Left column container with background--> */}
           <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
-            {/* <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
-              <img
-                src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                className="w-full"
-                alt="Sample image"
-              />
-            </div> */}
+       
 
             {/* <!-- Right column container --> */}
             <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12 bg-white p-5 rounded-md relative h-auto w-full mx-5 shadow-lg shadow-slate-200">
