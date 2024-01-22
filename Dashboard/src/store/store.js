81cf9cd7ee57fad4d12bbfdf7e16e46e379a9b53
yadamoon <yared241993@gmail.com>
2024-01-22 18:47:19 +0300

    remove some import file

--- Dashboard/src/store/store.js
+++ Dashboard/src/store/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import controllerSlice from "../../featureres/Slice/controllerSlice/controllerSlice";
 import authSlice from "./Slice/authSlice";
 
 export default configureStore({
