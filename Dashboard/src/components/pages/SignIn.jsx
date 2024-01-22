379be77ec85bfdf53f9ede70a47e16df269071fc
yadamoon <yared241993@gmail.com>
2024-01-22 23:29:28 +0300

    imported useSelector to access redux and i called signIn status

--- Dashboard/src/components/pages/SignIn.jsx
+++ Dashboard/src/components/pages/SignIn.jsx
@@ -6,7 +6,7 @@ import { useForm } from "react-hook-form";
 import { Link , useNavigate } from "react-router-dom";
 import auth from '../../services/http/auth'
 import { useDispatch } from 'react-redux'
-import { setStatus ,setUser } from "../../store/Slice/authSlice";
+import { setStatus ,setUser  } from "../../store/Slice/authSlice";
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
   const [showOrHiden, setshowOrHiden] = useState("show");
