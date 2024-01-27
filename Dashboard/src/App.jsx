3d4232909c689b79ee29f3df209bfd6e40723859
yadamoon <yared241993@gmail.com>
2024-01-27 17:10:55 +0300

    sth updated

--- Dashboard/src/App.jsx
+++ Dashboard/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 ">
           <SideBar />
         </div>
       </aside>
--- Dashboard/src/main.jsx
+++ Dashboard/src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import httpInit from './services/http/httpInit.js'
-
 import { Provider } from "react-redux";
 import store from "./app/store/store.js";
 httpInit()
