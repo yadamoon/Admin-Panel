d3b22460245c13d45a54d09e3d5d1697e3d3940d
yadamoon <yared241993@gmail.com>
2024-01-29 17:15:40 +0300

    i updated the import part

--- Dashboard/src/main.jsx
+++ Dashboard/src/main.jsx
@@ -4,15 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import httpInit from './services/http/httpInit.js'
-import { Provider } from "react-redux";
-import store from "./app/store/store.js";
+import { Provider } from 'react-redux';
+import { store } from "./store/store.js";
+
 httpInit()
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <BrowserRouter>
-    <Provider store={store}>
+  <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>
