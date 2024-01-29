273c406dcefa26c6fafd15da3651a97cbf579d6c
yadamoon <yared241993@gmail.com>
2024-01-29 17:36:19 +0300

    i updated before logIn hide my profile and add if login successfuly add for logOut some link

--- Dashboard/src/SideBar/common/HeaderApp.jsx
+++ Dashboard/src/SideBar/common/HeaderApp.jsx
@@ -34,7 +34,7 @@ export default function IndexPage() {
             <div className="flex items-end justify-end">
               {/* //!? button of signIn and SignUp */}
               <div className="">
-                <div className=" grid grid-cols-2 gap-2 text-center mx-3 ">
+                <div className=" grid grid-cols-3 gap-2 text-center mx-3 ">
                 {!signedIn && (
                   <Link
                     to="/SignIn"
@@ -51,6 +51,14 @@ export default function IndexPage() {
                     SignUp
                   </Link>
   )} 
+           {/* {signedIn && ( 
+                  <Link
+                    to="/SignUp"
+                    className="inline-block  bg-red-500 text-white border rounded-lg  p-2 pl-5 pr-5  text-sm font-medium uppercase hover:opacity-75 hover:text-white"
+                  >
+                    LogOut
+                  </Link>
+  )}  */}
   </div>
               </div>
 
@@ -109,7 +117,7 @@ export default function IndexPage() {
                   ></div>
                 </div>
               </div>
-
+              {signedIn && (
               <div className="flex ">
                 <div className="hidden xl:flex  justify-end items-end ">
                   <div className="">
@@ -179,6 +187,25 @@ export default function IndexPage() {
                             </svg>
                             <span className="ml-2"> Settings</span>
                           </li>
+                          <li className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-2 py-2 hover:text-indigo-700 flex items-center focus:text-indigo-700 focus:outline-none">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="icon icon-tabler icon-tabler-settings"
+                              width={20}
+                              height={20}
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              fill="none"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            >
+                              <path stroke="none" d="M0 0h24v24H0z" />
+                              <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                              <circle cx={12} cy={12} r={3} />
+                            </svg>
+                            <span className="ml-2"> LogOut</span>
+                          </li>
                         </ul>
                       )}
                       <div className=" flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out">
@@ -209,6 +236,7 @@ export default function IndexPage() {
                   </div>
                 </div>
               </div>
+              )}
             </div>
           </div>
         </nav>
