328fd1b7a1bfb7a525a4d25bb189704bbd496ced
yadamoon <yared241993@gmail.com>
2024-01-29 17:18:18 +0300

    i updated sth

--- Dashboard/src/store/store.js
+++ Dashboard/src/store/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authSlice from "./Slice/authSlice";
+import { configureStore } from '@reduxjs/toolkit'
+import authSlice from './Slice/authSlice'
 
-export default configureStore({
+export const store = configureStore({
   reducer: {
     auth: authSlice,
   },
-});
+})
