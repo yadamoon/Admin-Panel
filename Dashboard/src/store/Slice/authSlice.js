6b4851b9e47c0f2b7b327f083197d83bd7dd46fb
yadamoon <yared241993@gmail.com>
2024-01-29 17:17:34 +0300

    i updated full authSlice

--- Dashboard/src/store/Slice/authSlice.js
+++ Dashboard/src/store/Slice/authSlice.js
@@ -1,42 +1,33 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-
-
-import { createSlice } from "@reduxjs/toolkit";
-import { tokenService, userService } from "../../services/storageService";
+import { createSlice } from '@reduxjs/toolkit'
+import { tokenService, userService } from '../../services/storageService'
 const initialState = {
   signedIn: !!tokenService.getToken(),
   user: userService.getUser(),
-  show: {
-    signIn: true,
-    signUp: true,
-    logOut: true,
-  },
-};
+}
 
 export const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
-    setStatus: (state, action) => {
-      state.signedIn = action.payload.status;
+    setStatus: (state, { payload }) => {
+      state.signedIn = payload.status
     },
 
     setUser: (state, { payload }) => {
-      state.user = payload.user;
-      userService.saveUser(payload.user);
+      state.user = payload.user
+      userService.saveUser(payload.user)
     },
     setShow: (state, { payload }) => {
-      state.show = { ...state, [action.payload]: true };
+      state.show = { ...state, [action.payload]: true }
     },
     setHide: (state, { payload }) => {
-      state.show = { ...state, [action.payload]: false };
+      state.show = { ...state, [action.payload]: false }
     },
-    
   },
-});
+})
 
-export const { setStatus, setUser, setShow, setHide } = authSlice.actions;
+export const { setStatus, setUser, setShow, setHide } = authSlice.actions
 
-
-export default authSlice.reducer;
+export default authSlice.reducer
