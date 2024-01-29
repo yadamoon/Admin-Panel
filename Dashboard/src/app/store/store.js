38a853620abd08b269ed723e90919a894a7f7bfb
yadamoon <yared241993@gmail.com>
2024-01-29 17:16:45 +0300

    i deleted the whole app folde

--- Dashboard/src/app/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import controllerSlice from "../../Features/Slice/controllerSlice/controllerSlice";
-
-export default configureStore({
-  reducer: {
-    controller: controllerSlice,
-  },
-});
