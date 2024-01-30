12ef1ffa1e70e8b9d3c768f336a796d4a2a7076c
yadamoon <yared241993@gmail.com>
2024-01-30 17:08:10 +0300

    modified the button and size of the card

--- Dashboard/src/pages/LandingPage.jsx
+++ Dashboard/src/pages/LandingPage.jsx
@@ -10,9 +10,9 @@ function LandingPage (){
   }}
  >
    
-<div className="flex w-2/3 h-screen mx-3 my-16 ">
+<div className="flex w-2/3 h-auto mx-3 my-16 ">
 
-    <div className="col-span-1  bg-white  h-auto">
+    <div className="h-auto bg-white ">
     <h1 className="text-center items-center text-3xl text-blue-900">Welcome to  Your  Fav Site</h1>
    <div className="mx-3 justify-center items-center my-10  ">
    {/* Img  */}
@@ -20,19 +20,18 @@ function LandingPage (){
         <img src="public/La.png" alt="some advert image" className="w-full h-96" />
     </div>
     {/* some descrption */}
-    <p className="text-gray-500 bg-white">
+    <p className="text-gray-500 bg-white mx-10">
 
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus at qui aperiam incidunt sunt eaque iste sed. Dolores obcaecati, ullam ducimus, praesentium eaque corrupti veniam, debitis error dolorem iste dicta.
   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, voluptas doloremque! Saepe, dolorum architecto natus ullam unde dignissimos praesentium repudiandae corrupti odit eos quis omnis veritatis quasi. Sunt, assumenda quos. 
     </p>
     {/* buttons */}
    <div className="flex items-center justify-center">
-   <button className="bg-blue-500 text-gray-100 p-2 pr-2 pl-2  flex justify-center items-center">
+   <button className="bg-blue-500 text-gray-100 p-3 pr-5 pl-5  flex justify-center items-center rounded-md hover:opacity-75">
         GET START
     </button>
    </div>
    </div>
-
 
 
     </div>
