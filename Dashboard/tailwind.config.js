020e85e8671f33026a029a3e9d40cb48d7faca7f
yadamoon <yared241993@gmail.com>
2024-01-31 17:33:23 +0300

    add some color

--- Dashboard/tailwind.config.js
+++ Dashboard/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+            'top-color': '#8e4d57',
+            'heading': '#fcfbfc',
+          }
+     
+    },
   },
   plugins: [],
 };
