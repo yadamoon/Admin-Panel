d3b9ef37714c4f4f70c62bc93bf01400dc68dbb4
yadamoon <yared241993@gmail.com>
2024-02-09 23:50:19 +0300

    updated product list UI

--- Dashboard/src/pages/Products.jsx
+++ Dashboard/src/pages/Products.jsx
@@ -138,10 +138,124 @@ const Products = () => {
               <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pb-6 gap-8 ">
               {React.Children.toArray(items.map(item => 
                
+               <div  role="cell" className="bg-white shadow border">
+                 <div className="bg-white  shawo p-5 rounded-md relative h-full w-full">
+                  
+                   <span>
+                     <img
+                       className="bg-gray-200 p-2 mb-5 rounded-full"
+                       src={item.src}
+                       alt={item.msg}
+                     />
+                   </span>
+                   <h1 className="pb-4 text-2xl font-semibold">
+                     {item.name}
+                   </h1>
+                   <div className="my-5">
+                     <div className="flex items-center pb-4 dark:border-gray-700 cursor-pointer w-full space-x-3 hover:opacity-60">
+                       <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                         width="12.5"
+                         height={16}
+                         fill="none"
+                         viewBox="0 0 24 24"
+                         stroke="currentColor"
+                       >
+                         <path
+                           strokeLinecap="round"
+                           strokeLinejoin="round"
+                           strokeWidth={2}
+                           d="M9 5l7 7-7 7"
+                         />
+                       </svg>
+                       <h4 className="text-md text-gray-900 ">
+                         First time, what do I do next?
+                       </h4>
+                     </div>
+                     <div className="flex items-center pb-4 dark:border-gray-700 cursor-pointer w-full space-x-3">
+                       <div>
+                         <svg
+                           xmlns="http://www.w3.org/2000/svg"
+                           width="12.5"
+                           height={16}
+                           fill="none"
+                           viewBox="0 0 24 24"
+                           stroke="currentColor"
+                         >
+                           <path
+                             strokeLinecap="round"
+                             strokeLinejoin="round"
+                             strokeWidth={2}
+                             d="M9 5l7 7-7 7"
+                           />
+                         </svg>
+                       </div>
+                       <h4 className="text-md text-gray-900 ">
+                         Changing you profile picture and other information
+                       </h4>
+                     </div>
+
+                  <div className="flex items-center pb-4 dark:border-gray-700 cursor-pointer w-full">
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="12.5"
+        height={16}
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 5l7 7-7 7"
+        />
+      </svg>
+    </div>
+    <h4 className="text-md text-gray-900  pl-4">
+      What is the refund policy if I have to cancel during
+      the month
+    </h4>
+  </div> 
+                   </div>
+                   <a
+                     className="hover:text-indigo-500 hover:underline absolute bottom-5 text-sm text-indigo-700 font-bold cursor-pointer flex items-center"
+                     href="#"
+                   >
+                     <p>Show All</p>
+                     <div>
+                       <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                         className="icon icon-tabler icon-tabler-arrow-narrow-right"
+                         width={16}
+                         height={16}
+                         viewBox="0 0 24 24"
+                         strokeWidth="1.5"
+                         stroke="#4338CA"
+                         fill="none"
+                         strokeLinecap="round"
+                         strokeLinejoin="round"
+                       >
+                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                         <line x1={5} y1={12} x2={19} y2={12} />
+                         <line x1={15} y1={16} x2={19} y2={12} />
+                         <line x1={15} y1={8} x2={19} y2={12} />
+                       </svg>
+                     </div>
+                   </a>
+                 </div>
+               </div>
+             ))}
+
+
+
+              {/* {React.Children.toArray(items.map(item => 
+               
                   <div>
                     <div className="bg-white shadow-lg  rounded-md relative h-full w-full">
-                      {/* class="absolute inset-0 object-center object-cover h-full w-full"  */}
-                      <span className="hover:opacity-75 w-full ">
+                     
+                      <span className="hover:opacity-75 w-full  bg-white shadow">
                         <img
                           className="p-2 mb-5  w-full"
                           src={item.src}
@@ -153,7 +267,8 @@ const Products = () => {
                       </h1>
                     </div>
                   </div>
-                ))}
+                ))} */}
+                
               </div>
             </div>
           </div>
