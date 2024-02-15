c494089f0332a93758f8902443c507e1480b0214
yadamoon <yared241993@gmail.com>
2024-02-16 00:55:17 +0300

    updated code for shortly

--- Dashboard/src/controller/ProtectedRoute.jsx
+++ Dashboard/src/controller/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
-import { Route, Navigate } from 'react-router-dom';
+import {  Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-const ProtectedRoute = ({ element, ...rest }) => {
+const ProtectedRoute = ({ element }) => {
   const { signedIn } = useSelector((state) => state.auth);
 
-  return signedIn ? <Route {...rest} element={element} /> : <Navigate to="/signin" />;
+  return signedIn ?  element  : <Navigate to="/signin" />;
 };
 
 export default ProtectedRoute;
