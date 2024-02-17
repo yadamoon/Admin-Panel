8b64ec97d183c9044ed61c2bf338ba66d1327983
yadamoon <yared241993@gmail.com>
2024-02-17 17:10:28 +0300

    updated to cleanly the code and secure on admin and users

--- Dashboard/src/controller/ProtectedRoute.jsx
+++ Dashboard/src/controller/ProtectedRoute.jsx
@@ -1,11 +1,31 @@
+/* eslint-disable no-unreachable */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import {  Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import Settings from '../pages/Settings';
+import Products from '../pages/Products';
+import DashboardApp from '../pages/DashboardApp';
+import Profiles from '../pages/Profiles';
 
-const ProtectedRoute = ({ element }) => {
-  const { signedIn } = useSelector((state) => state.auth);
+const ProtectedRoute = ({ element , roles=[] }) => {
+  const { signedIn  , user} = useSelector((state) => state.auth);
+  if (signedIn) {
+    // Check if the user is an admin
+    if (roles.includes('*') || roles.includes(user.role) ) {
+      return <>{element}</>; // Render all components for the admin
+    } else {
+      return <Navigate to="/dashboard"/>
+    }
+  } else {
+    return <Navigate to="/signin" />; 
+  }
 
-  return signedIn ?  element  : <Navigate to="/signin" />;
+  return null;
+
 };
+
+
+
 
 export default ProtectedRoute;
