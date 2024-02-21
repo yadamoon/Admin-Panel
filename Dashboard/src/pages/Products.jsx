1abb5a5eddd53882d3de969fd5791eb92500ba36
yadamoon <yared241993@gmail.com>
2024-02-21 21:01:20 +0300

    updated something

--- Dashboard/src/pages/Products.jsx
+++ Dashboard/src/pages/Products.jsx
@@ -1,59 +1,136 @@
-import React,{useState} from 'react';
-const Products = () => {
-  const [showScroll, setShowScroll] = useState(false);
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+import { Form, useForm } from 'react-hook-form'
+import { http } from "../services/http/http"
 
- const handleMouseEnter = () => {
-    setShowScroll(true);
-  };
+const Products = () => {
+  const [showScroll, setShowScroll] = useState(false)
+  const navigate = useNavigate();
+
+
+  const handleMouseEnter = () => {
+    setShowScroll(true)
+  }
 
   const handleMouseLeave = () => {
-    setShowScroll(false);
-  };
+    setShowScroll(false)
+  }
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    // eslint-disable-next-line no-unused-vars
+    reset,
+  } = useForm({
+    defaultValues: {
+    task:''
+   
+    },
+  });
+  const addNewTask = async ({ task}) => {
+    const result = await http.request({
+      method: 'post',
+      url: 'tasks',
+      data: { task },
+    })
+    if (!result.isError) {
+      reset()
+      navigate('/Products')
+   
+      console.log(result)
+    }
+   
+ 
+  }
+
   const items = [
     {
-      name: " Account Overview",
-      src: "https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph",
-      alt: "home-1",
-      msg: "",
+      name: ' Account Overview',
+      src: 'https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph',
+      alt: 'home-1',
+      msg: '',
     },
     {
-      name: " Subscription Plans",
-      src: "https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph",
-      alt: "pricetags-1",
-      msg: "",
+      name: ' Subscription Plans',
+      src: 'https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph',
+      alt: 'pricetags-1',
+      msg: '',
     },
     {
-      name: "Payment Options",
-      src: "https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph",
-      alt: "home-1",
-      msg: "",
+      name: 'Payment Options',
+      src: 'https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph',
+      alt: 'home-1',
+      msg: '',
     },
     {
-      name: " Notification Settings",
-      src: "https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph",
-      alt: "home-1",
-      msg: "",
+      name: ' Notification Settings',
+      src: 'https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph',
+      alt: 'home-1',
+      msg: '',
     },
     {
-      name: "Profile Preferences",
-      src: "https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph",
-      alt: "home-1",
-      msg: "",
+      name: 'Profile Preferences',
+      src: 'https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph',
+      alt: 'home-1',
+      msg: '',
     },
     {
-      name: " Privacy and Cookies",
-      src: "https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph",
-      alt: "home-1",
-      msg: "",
+      name: ' Privacy and Cookies',
+      src: 'https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1703721600&semt=sph',
+      alt: 'home-1',
+      msg: '',
     },
-  ];
+  ]
+
 
   return (
-    <div   
-    onMouseEnter={handleMouseEnter}
-    onMouseLeave={handleMouseLeave}
-    style={{ overflow: showScroll ? 'auto' : 'hidden' }}>
-      <div className="bg-white w-full h-full" > 
+    <div
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      style={{ overflow: showScroll ? 'auto' : 'hidden' }}
+    >
+      <div className="bg-white w-full h-full">
+        <form className=" mt-4 space-y-4 lg:mt-5 md:space-y-5 ">
+          {/* <!-- Separator between social media sign in and email/password sign in --> */}
+          <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+            <p className="mx-4 mb-0 text-center font-bold text-xl text-blue-600">
+              Create New Task
+            </p>
+          </div>
+          {/* <!-- first Name --> */}
+          <div
+            className="relative mb-6 justify-center items-center text-center"
+            data-te-input-wrapper-init
+          >
+            <input
+              type="text"
+              {...register('task', {
+                required: 'Enter New Task please!',
+              })}
+              className=" w-2/3 h-32 rounded border border-gray-200 bg-transparent    "
+              // placeholder="Create new task"
+            />
+            {errors.task && (
+              <span className="text-red-700 col-span-1 md:col-span-2 ">
+                {errors.task.message}
+              </span>
+            )}
+          </div>
+          {/* <!-- Login button --> */}
+          <div className="text-center  ">
+            <button
+              type="button"
+              className="inline-block  bg-blue-500 text-white border rounded-lg  p-2 pl-10 pr-10  text-sm font-medium uppercase hover:opacity-75 hover:text-white"
+           onClick={handleSubmit(addNewTask)}
+            >
+              Done
+            </button>
+          </div>
+        </form>
+
         <div className="container mx-auto">
           <div role="article" className="bg-white py-12 md:px-8">
             <div className="px-4 xl:px-0 py-10">
@@ -61,7 +138,7 @@ const Products = () => {
                 <div className="mt-4 lg:mt-0 lg:w-3/5">
                   <div>
                     <h1 className="text-3xl ml-2 lg:ml-0 lg:text-4xl font-bold text-blue-600 underline tracking-normal lg:w-11/12">
-                      ProducDts
+                   Tasks
                     </h1>
                   </div>
                 </div>
@@ -86,7 +163,7 @@ const Products = () => {
                         x="0px"
                         y="0px"
                         viewBox="0 0 56.966 56.966"
-                        style={{ enableBackground: "new 0 0 56.966 56.966" }}
+                        style={{ enableBackground: 'new 0 0 56.966 56.966' }}
                         xmlSpace="preserve"
                         width="512px"
                         height="512px"
@@ -98,35 +175,33 @@ const Products = () => {
                 </div>
               </div>
             </div>
-            <div className="px-6 xl:px-0 justify-center items-center flex "   >
+            <div className="px-6 xl:px-0 justify-center items-center flex ">
               <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pb-6 gap-8 justify-center items-center">
-              {React.Children.toArray(items.map(item => 
-               
-               <div  role="cell" className="bg-white shadow border ">
-                 <div className="bg-white  shawo p-5 rounded-md relative h-full w-full">
-                  
-                   <span>
-                     <img
-                       className="bg-gray-200 p-2 mb-5 rounded-full"
-                       src={item.src}
-                       alt={item.msg}
-                     />
-                   </span>
-                   <h1 className="pb-4 text-2xl font-semibold text-center">
-                     {item.name}
-                   </h1>
-                 
-                 </div>
-               </div>
-             ))}
-
+                {React.Children.toArray(
+                  items.map((item) => (
+                    <div role="cell" className="bg-white shadow border ">
+                      <div className="bg-white  shawo p-5 rounded-md relative h-full w-full">
+                        <span>
+                          <img
+                            className="bg-gray-200 p-2 mb-5 rounded-full"
+                            // src={item.src}
+                            // alt={item.msg}
+                          />
+                        </span>
+                        <h1 className="pb-4 text-2xl font-semibold text-center text-blue-500">
+                          {item.name}
+                        </h1>
+                      </div>
+                    </div>
+                  ))
+                )}
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Products;
+export default Products
