6bf3f2077701ad92cbf86ecd0e90d799521a77d4
yadamoon <yared241993@gmail.com>
2024-02-28 13:26:22 +0300

    added to product and add new idea link to headerApp for phone size

--- Dashboard/src/SideBar/common/HeaderApp.jsx
+++ Dashboard/src/SideBar/common/HeaderApp.jsx
@@ -490,7 +490,29 @@ export default function IndexPage() {
                           </div>
                         </li>
                       </Link>
-                      {/* <Link to="/Product" className="cursor-pointer">
+                        <Link to="/NewIdea" className="cursor-pointer">
+                        <li className="text-gray-800 pt-8">
+                          <div className="flex items-center justify-between">
+                            <div className="flex items-center">
+                              <div className="w-6 h-6 md:w-8 md:h-8 text-gray-800">
+                                <svg
+                                  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-500 dark:group-hover:text-gray-500"
+                                  aria-hidden="true"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="currentColor"
+                                  viewBox="0 0 18 20"
+                                >
+                                  <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
+                                </svg>
+                              </div>
+                              <p className="text-gray-800 xl:text-base md:text-2xl text-base ml-3">
+                                New Idea
+                            </p>
+                            </div>
+                          </div>
+                        </li>
+                      </Link> 
+                      <Link to="/Product" className="cursor-pointer">
                         <li className="text-gray-800 pt-8">
                           <div className="flex items-center justify-between">
                             <div className="flex items-center">
@@ -511,7 +533,7 @@ export default function IndexPage() {
                             </div>
                           </div>
                         </li>
-                      </Link> */}
+                      </Link>
 
                       <a className="cursor-pointer">
                         <li className="text-gray-800 pt-8">
