f06c39b4b4d8b43555cc74b29bcdfba48957a5ed
yadamoon <yared241993@gmail.com>
2024-02-28 13:21:35 +0300

    uncomment the link of product and change the position

--- Dashboard/src/SideBar/SideBar.jsx
+++ Dashboard/src/SideBar/SideBar.jsx
@@ -115,8 +115,24 @@ const SideBar = () => {
                 </Link>
               </motion.li>
             )}
+                {/* add new Idea */}
+     {signedIn && (
+              <motion.li
+                whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
+                transition={{ type: 'spring', stiffness: 300 }}
+              >
+                <Link
+                  to="/NewIdea"
+                  className="flex items-center p-2 text-gray-500 transition duration-75 rounded-lg group hover:bg-blue-500 hover:text-white dark:hover:bg-gray-200"
+                >
+                 <i class="fa fa-lightbulb"></i>
+                  <span className="flex-1 ms-3 whitespace-nowrap">Add Idea</span>
+                </Link>
+              </motion.li>
+            )}
+
             {/* products */}
-            {/* {signedIn && (
+            {signedIn && (
               <motion.li
                 whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
                 transition={{ type: 'spring', stiffness: 300 }}
@@ -140,7 +156,7 @@ const SideBar = () => {
                   </span>
                 </Link>
               </motion.li>
-            )} */}
+            )}
             {/* settings */}
             {signedIn && (
               <motion.li
@@ -158,21 +174,7 @@ const SideBar = () => {
                 </Link>
               </motion.li>
             )}
-     {signedIn && (
-              <motion.li
-                whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
-                transition={{ type: 'spring', stiffness: 300 }}
-              >
-                <Link
-                  to="/NewIdea"
-                  className="flex items-center p-2 text-gray-500 transition duration-75 rounded-lg group hover:bg-blue-500 hover:text-white dark:hover:bg-gray-200"
-                >
-                 <i class="fa fa-lightbulb"></i>
-                  <span className="flex-1 ms-3 whitespace-nowrap">Add Idea</span>
-                </Link>
-              </motion.li>
-            )}
-
+        
             {signedIn && (
               <motion.li
                 whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
